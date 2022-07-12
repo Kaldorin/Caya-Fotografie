416123ec9bef406abf77b11402fbde8e5b536de7
Kaldorin <32927841+Kaldorin@users.noreply.github.com>
2022-07-12 22:01:50 +0200

    init

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+
+
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+  }
